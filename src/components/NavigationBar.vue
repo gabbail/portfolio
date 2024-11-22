79dc471c015f34d9be53ea5ffb94f78bfc8b3a61
Gabriel Baillargeon <gabrielbaillargeonsd@gmail.com>
2024-11-21 21:22:42 -0500

    Fixed navigation links

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -4,22 +4,22 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn text href="/">Home</v-btn>
-    <v-btn text href="/portfolio/about">About</v-btn>
+    <v-btn to="/" text>Home</v-btn>
+    <v-btn to="/about" text>About</v-btn>
     <v-menu open-on-hover>
       <template v-slot:activator="{ props }">
         <v-btn v-bind="props">Projects</v-btn>
       </template>
       <v-list>
-        <v-list-item href="/portfolio/work">
+        <v-list-item to="/work">
           <v-list-item-title>Work</v-list-item-title>
         </v-list-item>
-        <v-list-item href="/portfolio/projects">
+        <v-list-item to="/projects">
           <v-list-item-title>Personal</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
-    <v-btn text href="/portfolio/contact">Contact</v-btn>
+    <v-btn to="/contact" text>Contact</v-btn>
   </v-app-bar>
 </template>
 
@@ -36,5 +36,10 @@ export default {};
   font-style: normal;
   font-size: 40px;
   line-height: 50px;
+}
+
+.router-link {
+  color: red !important; /* Customize the color */
+  text-decoration: none; /* Optional: Remove underline */
 }
 </style>
